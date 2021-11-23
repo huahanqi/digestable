48fc7bf1b74ee9f381f06bc7e69dd5064e953da9
David <borland@renci.org>
2021-11-22 19:46:25 -0500

    Add react bootstrap

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+import { Container, Navbar, Row, Col } from 'react-bootstrap';
+
+const { Brand } = Navbar;
 
 function App() {
   return (
-    <div>
-      diges<b>table</b>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Brand>
+          <div className="ms-3">diges<b>table</b></div>
+        </Brand>
+      </Navbar>
+      <Container fluid>      
+        <Row>
+          <Col>
+            <div className="ms-3">hello</div>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
