d588871cfe6ac0bb377969c4d83e7aed2cb7c48e
David <borland@renci.org>
2021-11-23 20:10:40 -0500

    Add column sorting

--- src/digestable/digestable.js
+++ src/digestable/digestable.js
@@ -4,14 +4,16 @@ import './digestable.css';
 export const digestable = () => {
   function digestable(selection) {
     let table = d3.select(),        
-        data = null,
+        inputData = [],
+        data = [],
+        columns = [],
         
         // Parameters
         paddingX = 5,
         paddingY = 0;
 
     selection.each(function(d) {
-      data = d;
+      inputData = d;
 
       // Create skeletal table
       table = d3.select(this).selectAll('table')
@@ -27,8 +29,49 @@ export const digestable = () => {
           }
         );
 
+      createColumns();
+      sortData();
       drawTable();
     });
+
+    function clearSorting() {    
+      // XXX: Could just use find?  
+      columns.forEach(d => d.sort = null);
+    }
+
+    function createColumns() {
+      columns = inputData.columns.map(d => ({ name: d }));
+
+      clearSorting();
+    }
+
+    function sortByColumn(column) {    
+      const sort = column.sort === 'ascending' ? 'descending' : 'ascending';
+      
+      clearSorting();
+
+      column.sort = sort;
+
+      sortData();
+    }
+
+    function sortData() {
+      const sortColumn = columns.find(({ sort }) => sort !== null);
+
+      if (sortColumn) {
+        const { name, sort } = sortColumn;
+
+        data = inputData.sort((a, b) => {
+          return d3[sort](a[name], b[name]);
+        });
+      }
+      else {
+        data = [...inputData];
+      }
+    }
+
+    function processData() {
+    }
 
     function drawTable() {
       const px = paddingX + 'px';
@@ -39,13 +82,18 @@ export const digestable = () => {
 
       function drawHeader() {
         table.select('thead').select('tr').selectAll('th')
-          .data(data.columns)
+          .data(columns, d => d.name)
           .join('th')
           .style('padding-left', px)
           .style('padding-right', px)
           .style('padding-top', py)
           .style('padding-bottom', py)
-          .text(d => d);
+          .text(d => d.name)
+          .on("click", (evt, d) => {
+            console.log(d);
+            sortByColumn(d);
+            drawTable()
+          });
       }
 
       function drawBody() {
@@ -54,13 +102,13 @@ export const digestable = () => {
           .join('tr')
           .each(function(d) {
             d3.select(this).selectAll('td')
-              .data(data.columns)
+              .data(columns)
               .join('td')
               .style('padding-left', px)
               .style('padding-right', px)
               .style('padding-top', py)
               .style('padding-bottom', py)
-              .text(column => d[column]);
+              .text(column => d[column.name]);
           })          
       }
     }  
