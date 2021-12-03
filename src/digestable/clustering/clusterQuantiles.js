86d97e1e30d594c736dadce233bf6353c5a7100f
David <borland@renci.org>
2021-12-03 15:42:34 -0500

    Add quantile clustering

---
+++ src/digestable/clustering/clusterQuantiles.js
@@ -0,0 +1,14 @@
+import * as d3 from 'd3';
+
+export default function clusterQuantiles(values, n) {
+  const range = d3.range(n);
+
+  const scale = d3.scaleQuantile()
+    .domain(values)
+    .range(range);
+
+  return values.reduce((clusters, value, i) => {
+    clusters[scale(value)].push(i);
+    return clusters;
+  }, range.map(() => []));
+}
