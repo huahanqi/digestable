97abc0bf617b0ec7b077cc37ec1eaf30176b5637
David <borland@renci.org>
2021-12-23 14:32:31 -0500

    Add file select

--- src/app.js
+++ src/app.js
@@ -6,23 +6,22 @@ import { TableWrapper } from './components/table-wrapper';
 import { SimplifyControls, VisualizationControls } from './components/controls';
 
 const { Brand } = Navbar;
-const { Control } = Form;
+const { Group, Control, Select } = Form;
+
+const datasets = [
+  { name: 'MT Cars', url: 'data/mtcars.csv' },
+  { name: 'NFL Combine 2017', url: 'data/NFL Combine 2017.csv' }
+];
 
 export const App = () => {
+  const [dataset, setDataset] = useState('');
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
 
   // XXX: Add loading spinner
-
-  const onFileSelect = async evt => {
-    const file = evt.target.files.length === 1 ? evt.target.files[0] : null;
-
-    if (!file) return;
-
+  const loadData = async url => {
     setData(null);
     setLoading(true);
-
-    const url = URL.createObjectURL(file);
 
     try {
       const csvData = await d3.csv(url);
@@ -32,6 +31,22 @@ export const App = () => {
     }
     catch (error) {
       console.log(error);
+    }
+  }
+
+  const onSelectChange = evt => {
+    const url = evt.target.value;
+    
+    setDataset(url);
+    loadData(url);
+  };
+
+  const onFileSelect = evt => {
+    const file = evt.target.files.length === 1 ? evt.target.files[0] : null;
+
+    if (file) {
+      setDataset('');
+      loadData(URL.createObjectURL(file));
     }
   };
 
@@ -49,11 +64,36 @@ export const App = () => {
           <span className='text-align-bottom'>diges<b>table</b></span>
         </Brand>        
         <Form>
-          <Control 
-            type='file'
-            accept='.csv'
-            onChange={ onFileSelect }
-          />
+          <Group as={ Row } className='align-items-center'>
+            <Col>
+              <Select 
+                value={ dataset }
+                onChange={ onSelectChange }
+              >
+                <option
+                  value=''
+                  disabled
+                >
+                  Choose dataset
+                </option>
+                { datasets.map((dataset, i) => (
+                  <option key={ i } value={ dataset.url }>
+                    { dataset.name }
+                  </option> 
+                ))}
+              </Select>
+            </Col>
+            <Col sm='auto' className='text-center'>
+              <span className='text-light'>or</span>
+            </Col>
+            <Col>
+              <Control 
+                type='file'
+                accept='.csv'
+                onChange={ onFileSelect }
+              />
+            </Col>
+          </Group>
         </Form>
       </Navbar>
       { data ? 
